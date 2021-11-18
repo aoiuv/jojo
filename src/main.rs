a114948330389e5c4dc130276f7307f9f1df44ed
yanguangjie <yanguangjie@bytedance.com>
2021-11-19 00:28:46 +0800

    feat: initialize jo action parse

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,70 @@
 use std::env;
 use std::process;
+use std::error;
 
-mod jo;
+const JO_CFG: &str = "cfg.jo";
+
+#[derive(Debug)]
+pub struct Config {
+    pub action: Action,
+    pub param: String,
+}
+
+#[derive(Debug)]
+pub enum Action {
+    Register,
+    UnRegister,
+    List,
+    Expand,
+    Clean,
+}
+
+fn dispatch(action: Action) {}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, String> {
+        if args.len() < 2 {
+            return Err("need jo action".to_string());
+        }
+        let action = args[1].clone();
+        if args.len() < 3 {
+            return Err("need jo param of action".to_string());
+        }
+        let param = args[2].clone();
+
+        match action.as_str() {
+            "register" | "r" => Ok(Config {
+                action: Action::Register,
+                param,
+            }),
+            "unregister" | "R" => Ok(Config {
+                action: Action::UnRegister,
+                param,
+            }),
+            "list" | "l" => Ok(Config {
+                action: Action::List,
+                param,
+            }),
+            "expand" | "e" => Ok(Config {
+                action: Action::Expand,
+                param,
+            }),
+            "clean" => Ok(Config {
+                action: Action::Clean,
+                param,
+            }),
+            _ => Err(format!("invalid action `{}`", action)),
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = jo::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+
+    let cfg = Config::new(&args).unwrap_or_else(|err| {
+        println!("[JOJO Error] {}", err);
         process::exit(1);
     });
-    
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-    
-    if let Err(e) = jo::run(config) {
-        println!("Application error: {}", e);
-        process::exit(1);
-    }
+
+    println!("Config: {:?}", cfg);
 }
